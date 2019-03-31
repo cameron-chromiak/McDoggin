62d68c3074beee8aa2ed1341da71554dfc78a655
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-30 21:10:20 -0400

    -added models -worked on user route

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+
+//Load routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
 
 const app = express()
 
@@ -10,10 +15,18 @@ mongoose.connect(db, { useNewUrlParser: true })
   .then( res => console.log('Mongo Connected'))
   .catch(err => console.log(err))
 
+  //Body Parser Middleware
+  app.use(bodyParser.urlencoded({extended: false}))
+  app.use(bodyParser.json())
+
 
 app.get('/', (req, res) =>{
   res.send('.kjl')
 })
+
+//Use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
 
 
 const port = process.env.PORT || 4000
---
+++ routes/api/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router()
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+//User model
+const User = require('../../models/User');
+
+//@route GET /api/users/test
+//@desc
+//@access Public
+router.get('/register', (req, res)=>{
+  let error = {}
+  user.findOne({email: req.body.email})
+  .then(user => {
+    if(user){
+      res.status(400).json({error: 'Email taken'})
+    }else{
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      })
+      bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(newUser.password, salt, (err, hash) =>{
+          if(err) throw err;
+          newUser.password = hash
+          newUser.save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err))
+        })
+      })
+    }
+  })
+})
+
+module.exports = router
